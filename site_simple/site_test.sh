02234789fa9f2112807c1642eacb9f9728fc179d
Ryan McGuinness <rmcguinness@google.com>
2022-08-30 11:15:57 -0400

    Updated Test script with new root

--- site_simple/site_test.sh
+++ site_simple/site_test.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 find .
 
 # Make a few assertions about the generated site
-test -f site/site/index.html
-test -f site/site/index.xml
+test -f site_simple/site_simple/index.html
+test -f site_simple/site_simple/index.xml
